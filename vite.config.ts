9109964f4324152558d8adae7f13437c34c47f25
Arya Eka Nugraha <narya9889@gmail.com>
2026-09-23 18:44:17 +0700

    build: update base path and fix build sequence for GHP
    Configure the base URL dynamically for GitHub Pages and ensure the build process runs after the environment setup.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: process.env.GITHUB_ACTIONS ? '/KKL_informatika_kelompok7/' : './',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
